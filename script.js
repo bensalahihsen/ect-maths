846c6c264fd490e15f35535a8d6768117c0671bd
bensalahihsen <ihsenbensalah5@gmail.com>
2026-03-14 18:20:49 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,75 +1,31 @@
-const filesData = {
+const folders = {
 
-    "Bloc1": [
-        {
-            name: "Cours2.pdf",
-            id: "1YfZDWMMZy5a4JeUUVRj5r91XroEmwkpG"
-        }
-    ],
-
-    "Bloc2": [],
-    "Bloc3": [],
-    "Bloc4": [],
-    "Bloc5": []
+    "Bloc1": "1nDQl93pMmLKA16KB-MjQmX-H1YWiJ9O0",
+    "Bloc2": "17yIfxM6NqiamyWm0uuBPugWaV0wfAQi5",
+    "Bloc3": "12r09E6w5LcKN9Q21gdr7XRfF7BfX5YFi",
+    "Bloc4": "17UwhQeTMhAo3449k6C4zskLMkaqogUfY",
+    "Bloc5": "17_aGxwB98JOB4jWBdxOAVL3FUZiSJkrA"
 
 };
 
 
-function openBloc(blocName) {
+function openBloc(blocName){
 
-    const grid = document.querySelector(".grid");
-    grid.innerHTML = "";
-
-    const files = filesData[blocName];
-
-    if (files.length === 0) {
-
-        grid.innerHTML = "<p>Aucun fichier dans ce bloc.</p>";
-
-    } else {
-
-        files.forEach(file => {
-
-            const card = document.createElement("div");
-            card.className = "card file-card";
-            card.innerText = file.name;
-
-            card.onclick = () => {
-                showFile(file.id);
-            };
-
-            grid.appendChild(card);
-
-        });
-
-    }
-
-    const backButton = document.createElement("div");
-    backButton.className = "card";
-    backButton.innerText = "⬅ Retour";
-    backButton.onclick = () => location.reload();
-
-    grid.appendChild(backButton);
-
-}
-
-
-function showFile(fileId) {
+    const folderId = folders[blocName];
 
     const grid = document.querySelector(".grid");
 
     grid.innerHTML = `
 
-        <iframe 
-        src="https://drive.google.com/file/d/${fileId}/preview"
-        width="100%" 
-        height="600px"
-        style="border:none;">
-        </iframe>
+    <iframe 
+    src="https://drive.google.com/embeddedfolderview?id=${folderId}#list"
+    style="width:100%; height:600px; border:0;">
+    </iframe>
 
-        <div class="card" onclick="location.reload()">
-        ⬅ Retour
-        </div>
+    <div class="card" onclick="location.reload()">
+    ⬅ Retour
+    </div>
 
     `;
+
 }
