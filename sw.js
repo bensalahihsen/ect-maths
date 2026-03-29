6f1ecf68d43a9f8eeebcacb54946f4b29ba32e9f
bensalahihsen <ihsenbensalah5@gmail.com>
2026-03-29 15:01:51 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "maths-app-v1";
+
+const urlsToCache = [
+  "index.html",
+  "style.css",
+  "script.js",
+  "logo.png"
+];
+
+// installation
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// fetch (offline)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
